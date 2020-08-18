7e9566c15674cf76886f443c6008d4144be2515a
ArieleM <ariele_apm@outlook.com>
2020-08-18 18:15:15 -0300

    Implementando o link do header

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,9 +3,7 @@ const homeController = {
     let servicos = [
       {nome: 'Dev full stack', imagem: '/imagens/undraw_dev_focus.svg'},
       {nome: 'Consultoria UX', imagem: '/imagens/undraw_mobile_apps.svg'},
-      {nome: 'Marketing Digital', imagem: '/imagens/undraw_social_dashboard.svg'},
-      {nome: 'Suporte tecnico', imagem: '/imagens/undraw_dev_focus.svg'},
-      {nome: 'Data Science', imagem: '/imagens/undraw_mobile_apps.svg'},
+      {nome: 'Marketing Digital', imagem: '/imagens/undraw_social_dashboard.svg'}
     ];
 
     let banners = [
